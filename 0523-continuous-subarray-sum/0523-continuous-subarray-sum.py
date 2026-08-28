7f17d69780bcb61565619121994ca709b6121bdd
SANIA186 <164989442+SANIA186@users.noreply.github.com>
2026-08-28 19:27:53 +0530

    Time: 55 ms (55.54%), Space: 31.1 MB (83.88%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def checkSubarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        remainder_index = {0: -1}
+        prefix = 0
+
+        for i, num in enumerate(nums):
+            prefix += num
+            rem = prefix % k
+
+            if rem in remainder_index:
+                if i - remainder_index[rem] >= 2:
+                    return True
+            else:
+                remainder_index[rem] = i
+
+        return False
