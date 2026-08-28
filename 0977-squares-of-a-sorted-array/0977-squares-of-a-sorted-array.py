adb50c9c45150e23cb4a781dca6a272733f62387
SANIA186 <164989442+SANIA186@users.noreply.github.com>
2026-08-28 19:32:00 +0530

    Time: 178 ms (5.08%), Space: 14.4 MB (9.42%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        for i in range(len(nums)):
+            nums[i] = nums[i] ** 2
+
+        
+        def merge_sort(arr):
+            if len(arr) <= 1:
+                return arr
+
+            mid = len(arr) // 2
+
+            left = merge_sort(arr[:mid])
+            right = merge_sort(arr[mid:])
+
+            return merge(left, right)
+
+        
+        def merge(left, right):
+            result = []
+            i = 0
+            j = 0
+
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    result.append(left[i])
+                    i += 1
+                else:
+                    result.append(right[j])
+                    j += 1
+
+            result.extend(left[i:])
+            result.extend(right[j:])
+
+            return result
+
+        return merge_sort(nums)
