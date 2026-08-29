da936d2d7018f0d446dd6ff05778282e679cf14e
SANIA186 <164989442+SANIA186@users.noreply.github.com>
2026-08-29 18:35:20 +0530

    Time: 10 ms (92.66%), Space: 12.2 MB (99.24%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a= 0
+        b=1
+        for i in range(n):
+            c= a+b
+            a=b
+            b=c
+        return a
+
+        
